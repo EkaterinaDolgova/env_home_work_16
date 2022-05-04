1fb6e867beabbd40dda4962d0b215ac2d617b8e1
katya <kotenokches@rambler.ru>
2022-05-04 08:31:18 +0500

    Initial commit

---
+++ src/main/java/sky/pro/env_home_work_16/EnvHomeWork16Application.java
@@ -0,0 +1,13 @@
+package sky.pro.env_home_work_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvHomeWork16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvHomeWork16Application.class, args);
+	}
+
+}
---
+++ src/test/java/sky/pro/env_home_work_16/EnvHomeWork16ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.env_home_work_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvHomeWork16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
