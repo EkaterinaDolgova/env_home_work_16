f3eba24fc28ab2b38a9f1562063286688a2ffb88
katya <kotenokches@rambler.ru>
2022-05-05 09:53:21 +0500

    ДЗ 16 выполнено-исправлено

--- src/main/java/sky/pro/env_home_work_16/StringListServiceImpl.java
+++ src/main/java/sky/pro/env_home_work_16/StringListServiceImpl.java
@@ -2,72 +2,110 @@ package sky.pro.env_home_work_16;
 
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
+
 
 @Service
 public class StringListServiceImpl implements StringListService {
-    private final ArrayList<String> lists;
+    private String[] lists;
 
     public StringListServiceImpl() {
-        this.lists = new ArrayList<String>();
-        lists.add("Tom");
-        lists.add("Alice");
-        lists.add("Kate");
-        lists.add("Sam");
+        this.lists = new String[3];
+        lists[0] = "Tom";
+        lists[1] = "Alice";
+        lists[2] = "Kate";
+        lists[3] = "Sam";
     }
 
 
     @Override
     public String add(String item) {
-        if (lists.contains(item)) {
+        boolean f_is = false;
+        Integer j = 0;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] == item) {
+                f_is = true;
+            }
+        }
+        if (f_is) {
             return "Дубль";
         }
-        lists.add(item);
-        return lists.toString();
+        j = lists.length + 1;
+        for (int i = lists.length - 1; i > j; i--) {
+            lists[i] = lists[i - 1];
+        }
+        lists[j] = item;
+        return lists[j];
     }
 
     @Override
     public String addIndex(int index, String item) {
-        if (index <= lists.size()) {
-            lists.add(index, item);
-            return lists.toString();
+        Integer j = 0;
+        if (index <= lists.length) {
+            j = lists.length + 1;
+            for (int i = lists.length - 1; i > j; i--) {
+                lists[i] = lists[i - 1];
+            }
+            lists[j] = item;
+            return lists[j];
         }
         throw new StringListNotFoundException("Индекс выходит за пределы фактического количества элементов массива");
     }
 
     @Override
     public String set(int index, String item) {
-        if (index <= lists.size()) {
-            lists.set(index, item);
-            return lists.toString();
+        if (index <= lists.length) {
+            lists[index] = item;
+            return lists[index] = item;
         }
         throw new StringListNotFoundException("Индекс выходит за пределы фактического количества элементов массива");
     }
 
     @Override
     public String remove(String item) {
-        if (lists.contains(item)) {
-            lists.remove(item);
-            return lists.toString();
+        boolean f_is = false;
+        Integer j = 0;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] == item) {
+                f_is = true;
+                j = i;
+            }
+        }
+        if (f_is) {
+            for (int i = j; i < lists.length - 1; i++) {
+                lists[i] = lists[i + 1];
+                return "Запись удалена " + lists[i];
+            }
+
         }
         return "Нет данных";
     }
 
     @Override
     public String removeIndex(int index) {
-        if (index <= lists.size()) {
-            lists.remove(index);
-            return lists.toString();
+        if (index <= lists.length) {
+            for (int i = index; i < lists.length; i++) {
+                if (lists[i] == lists[index]) {
+                    for (int j = index; j < lists.length; j++) {
+                        lists[i] = lists[j];
+                    }
+                }
+                lists = Arrays.copyOf(lists, lists.length - 1);
+                return "Удалена строка: " + lists[index];
+            }
         }
         throw new StringListNotFoundException("Индекс выходит за пределы фактического количества элементов массива");
     }
 
     @Override
     public boolean contains(String item) {
-        if (lists.contains(item)) {
-            lists.remove(item);
+        boolean f_is = false;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] == item) {
+                f_is = true;
+            }
+        }
+        if (f_is) {
             return true;
         }
         return false;
@@ -75,32 +113,52 @@ public class StringListServiceImpl implements StringListService {
 
     @Override
     public int indexOf(String item) {
-        if (lists.contains(item)) {
-            return lists.indexOf(item);
+        boolean f_is = false;
+        Integer j = 0;
+        for (int i = 0; i < lists.length; i++) {
+            if (lists[i] == item) {
+                f_is = true;
+                j = i;
+            }
+        }
+        if (f_is) {
+            return j;
         }
         return -1;
-
     }
 
     @Override
     public int lastIndexOf(String item) {
-        if (lists.contains(item)) {
-            return lists.lastIndexOf(item);
+        boolean f_is = false;
+        Integer j = 0;
+        for (int i = lists.length; i >= 0; i--) {
+            if (lists[i] == item) {
+                f_is = true;
+                j = i;
+            }
+        }
+        if (f_is) {
+            return j;
         }
         return -1;
     }
 
     @Override
     public String get(int index) {
-        if (index <= lists.size()) {
-            return lists.get(index);
+        if (index <= lists.length) {
+            return lists[index];
         }
         throw new StringListNotFoundException("Индекс выходит за пределы фактического количества элементов массива");
     }
 
     @Override
     public boolean equals(String[] otherList) {
-        if (lists.equals(otherList)) {
+        String[] lists_ = new String[3];
+        lists_[0] = "Tom";
+        lists_[1] = "Alice";
+        lists_[2] = "Kate";
+        lists_[3] = "Sam";
+        if (lists.equals(lists_)) {
             return true;
         }
         return false;
@@ -108,12 +166,12 @@ public class StringListServiceImpl implements StringListService {
 
     @Override
     public int size() {
-        return lists.size();
+        return lists.length;
     }
 
     @Override
     public boolean isEmpty() {
-        if (lists.isEmpty()) {
+        if (lists.length == 0) {
             return true;
         }
         return false;
@@ -121,13 +179,13 @@ public class StringListServiceImpl implements StringListService {
 
     @Override
     public void clear() {
-        lists.clear();
+        Arrays.fill(lists, null);
     }
 
     @Override
     public String[] toArray() {
-        String[] b = new String[lists.size()];
-        lists.toArray(b);
+        String[] b = new String[lists.length];
+        b = Arrays.copyOf(lists, lists.length);
         return b;
     }
 
